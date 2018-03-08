e0b3446480aa28ae6564aec61354494578a0aca1
Leo Belzile <leo.belzile@epfl.ch>
2018-03-08 11:12:51 +0100

    Scaling not taken into account

--- R/mvtrnd.R
+++ R/mvtrnd.R
@@ -11,9 +11,7 @@ mvtrnd <- function(n, L, l, u, nu, mu)
   Z <- matrix(0, nrow = d, ncol = n); # create array for variables
   # precompute constants
   const <- log(2*pi) / 2 - lgamma(nu/2) - (nu / 2 - 1) * log(2) + lnNpr(-eta, Inf) + 0.5 * eta^2;
-  R <- eta + trandn(rep(-eta, n), rep(Inf, n)); # simulate R~N(eta,1) with R>0
   p <- (nu - 1) * log(R) - eta * R + const; # compute Likelihood Ratio for R
-  R <- R / sqrt(nu); # scale parameter divided by nu
   for(k in 1:d){
     # compute matrix multiplication L*Z
     col <- L[k,1:k] %*% Z[1:k,];
@@ -21,7 +19,7 @@ mvtrnd <- function(n, L, l, u, nu, mu)
     tl <- R * l[k] / sqrt(nu) - mu[k] - col;
     tu <- R * u[k] / sqrt(nu) - mu[k] - col;
     #simulate N(mu,1) conditional on [tl,tu]
-    Z[k,] <- mu[k]+trandn(tl, tu);
+    Z[k,] <- mu[k] + trandn(tl, tu);
     # update likelihood ratio
     p <- p + lnNpr(tl,tu)  + 0.5*mu[k]^2 - mu[k] * Z[k,];
   }
