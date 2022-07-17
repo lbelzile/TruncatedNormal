3977fa039405ddad72a7c337e4c4b1975a9b5622
Anthony Vankus <anthonyvankus@gmail.com>
2022-07-17 16:49:51 -0400

    implementation of SOV estimators for covariance and precision matrix, no exponential tilting

---
+++ R/covarianceSOV.R
@@ -0,0 +1,27 @@
+covarianceSOV <-
+  function(Sig, l, u){
+    Lchol = t(chol(Sig));
+    d = length(l);
+    X = rep(0,d);
+    rem = 0;
+    
+    l[1] = l[1]/Lchol[1,1];
+    u[1] = u[1]/Lchol[1,1];
+    
+    X[1] = trandn(l[1], u[1]);
+    
+    for(j in (2:d)){
+      for(i in (1:(j-1))){
+        rem = rem + Lchol[j,i]*X[i];
+      }
+      l[j] = (l[j] - rem)/Lchol[j,j];
+      u[j] = (u[j] - rem)/Lchol[j,j];
+      
+      X[j] = trandn(l[j],u[j]);
+    }
+    est = 1;
+    for(k in (1:d)){
+      est = est * (pnorm(u[k]) - pnorm(l[k]));
+    }
+    return(est);
+  }
---
+++ R/precisionSOV.R
@@ -0,0 +1,43 @@
+precisionSOV <- 
+  function(SigmInv, l, u){
+    Tchol = t(chol(SigmInv));
+    d = length(l);
+    X = rep(0,d);
+    rem = 0;
+    
+    l[d] = Tchol[d,d]*l[d];
+    u[d] = Tchol[d,d]*u[d];
+    X[d] = trandn(l[d],u[d]);
+    
+    for(j in ((d-1):1)){
+      l[j] = l[j]*Tchol[j,j];
+      u[j] = u[j]*Tchol[j,j];
+      
+      for(i in ((j+1):d)){
+        rem = rem + (Tchol[i,j]*(X[i] - eta(i,d,Tchol,X[i])))/Tchol[i,i];
+      }
+      
+      l[j] = l[j] + rem;
+      u[j] = u[j] + rem;
+      
+      X[j] = trandn(l[j], u[j]);
+    }
+    est = 1;
+    for(k in (1:d)){
+      est = est * (pnorm(u[k]) - pnorm(l[k]));
+    }
+    return(est);
+  }
+
+eta <-
+  function(j, d, M, x){
+    eta = 0;
+    if(j == d){
+      return(0);
+    }
+    for(i in ((j+1):d)){
+      eta = eta + M[i,j]/M[i,i];
+    }
+    eta = (eta * x)/M[j,j];
+    return(eta);
+  }
