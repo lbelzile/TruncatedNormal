ac786a68cc5c2ea68ec8c18ab8bf2f99730f1789
Anthony Vankus <anthonyvankus@gmail.com>
2022-08-10 17:38:44 -0400

    modified precisionSOV according to new calculations and wrote SOVcomparisons to compare precisionSOV and covarianceSOV

--- R/precisionSOV.R
+++ R/precisionSOV.R
@@ -8,44 +8,38 @@ precisionSOV <-
       stop("Truncation limits have to be of the same length");
     }
     
-    Tchol = t(chol(SigInv));
+    Tchol = chol(SigInv);
+    
     d = length(l);
     X = rep(0,d);
-    rem = 0;
     
-    l[d] = Tchol[d,d]*l[d];
-    u[d] = Tchol[d,d]*u[d];
-    X[d] = trandn(l[d],u[d]);
+    X[d] = mvrandn(l[d],u[d],Tchol[d,d]^(-2),1, mu = 0);
     
     for(j in ((d-1):1)){
-      l[j] = l[j]*Tchol[j,j];
-      u[j] = u[j]*Tchol[j,j];
+      nu = sum(-(Tchol[j, (j+1):d] %*% X[(j+1):d])/Tchol[j,j]);
       
-      for(i in ((j+1):d)){
-        rem = rem + (Tchol[i,j]*(X[i] - eta(i,d,Tchol,X[i])))/Tchol[i,i];
+      
+      l[j] = (l[j] - nu);
+      u[j] = (u[j] - nu);
+      
+      
+      l[j] = l[j] - nu;
+      u[j] = u[j] - nu;
+      
+      if(j != 1){
+        X[j] = mvrandn(l[j], u[j], Tchol[j,j]^(-2), 1, mu = 0);
+       
       }
-      
-      l[j] = l[j] + rem;
-      u[j] = u[j] + rem;
-      
-      X[j] = trandn(l[j], u[j]);
     }
     est = 1;
+    
+    u = u*diag(SigInv);
+    l = l*diag(SigInv);
+   
     for(k in (1:d)){
       est = est * (pnorm(u[k]) - pnorm(l[k]));
     }
     return(est);
   }
 
-eta <-
-  function(j, d, M, x){
-    eta = 0;
-    if(j == d){
-      return(0);
-    }
-    for(i in ((j+1):d)){
-      eta = eta + M[i,j]/M[i,i];
-    }
-    eta = (eta * x)/M[j,j];
-    return(eta);
-  }
+
