37e97ff887ca9a0986f8dbe2ce350f39d2d88763
anthonyv <anthonyvankus@gmail.com>
2022-08-17 10:51:07 -0400

    working precision matrix SOV estimator

---
+++ precisionSOV.R
@@ -0,0 +1,55 @@
+precisionSOV <- 
+  function(SigInv, l, u, n = 10000){
+    if(any(l>u)){
+      stop("Lower bound needs to be smaller than upper bound");
+    }
+    
+    if(length(l)!=length(u)){
+      stop("Truncation limits have to be of the same length");
+    }
+    
+    Tchol = cholperm(SigInv, l, u)$L;
+    Tchol = t(Tchol);
+    
+    d = length(l);
+    
+    for(k in 1:d){
+      l[k] = Tchol[k,k]*l[k];
+      u[k] = Tchol[k,k]*u[k];
+    }
+    a = l;
+    b = u;
+    acc = 0;
+    
+    for(m in 1:n){
+      
+      x = rep(0,d);
+      nu = rep(0,d);
+      l = a;
+      u = b;
+    
+      x[d] = trandn(l[d],u[d])/Tchol[d,d];
+      
+      for(j in ((d-1):1)){
+        for(i in (j+1):d){
+          nu[j] = nu[j] + Tchol[j,i]*x[i];
+        }
+        
+        l[j] = l[j] + nu[j];
+        u[j] = u[j] + nu[j];
+        
+        if(j != 1){
+          x[j] = (trandn(l[j],u[j])-nu[j])/Tchol[j,j];
+        }
+      }
+      est = 1;
+      for(k in (1:d)){
+        est = est * (pnorm(u[k]) - pnorm(l[k]));
+      }
+      
+      acc = acc + est;
+    }
+    return(acc/n);
+  }
+
+
